7760534aeacb4f4b8f50c3c69a5527bebccd7485
Slavik Glazyrin <svyatoslav.gl@gmail.com>
2021-01-14 07:41:04 -0800

    Create explore.sql

---
+++ explore.sql
@@ -0,0 +1,19 @@
+USE <Report Server database>;
+
+SELECT
+	[ItemID]
+    ,[Path]
+    ,[Name]
+    ,[ParentID]
+    ,[Type]
+    ,[Description]
+    ,[Hidden]
+    ,[CreatedByID]
+    ,[CreationDate]
+    ,[ModifiedByID]
+    ,[ModifiedDate]
+    ,[PolicyID]
+    ,[PolicyRoot]
+    ,[ContentSize]
+FROM 
+	[dbo].[Catalog]
